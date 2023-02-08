c6618bed901b7d3752b09e4d8a95521b6ff32cfd
Roketskyi Roman <91413976+Roketskyi@users.noreply.github.com>
2023-02-08 08:52:50 +0200

    Add files via upload

--- scripts/insert-div.js
+++ scripts/insert-div.js
@@ -1,5 +1,15 @@
 let div = document.createElement('div');
-
 div.innerHTML = "<a href='#contacts' class='header-btn box'><strong>Безкоштовна консультація</strong></a>";
-
 document.documentElement.clientWidth > 576 ? divv.append(div) : phone.append(div);
+
+let p = document.createElement('p');
+p.className = "viber-p"
+p.innerHTML = "Задай питання у Viber";
+divViber.prepend(p)
+
+let messageViber = document.querySelector('.viber');
+let triangleViber = document.querySelector('#triangle')
+messageViber.onmouseover = function(event) {
+    event.target = (setTimeout(() => p.remove(), 200))
+    event.target = (setTimeout(() => triangleViber.remove(), 200))
+};

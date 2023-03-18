939dbbba344488910710a4840f8fcf87dc7cd9ba
Roketskyi Roman <91413976+Roketskyi@users.noreply.github.com>
2023-03-18 12:35:12 +0200

    update 1.8

--- scripts/drop-down.js
+++ scripts/drop-down.js
@@ -19,3 +19,28 @@ circleButton.addEventListener('click', () => {
     buttonsVisible = true;
   }
 });
+
+// Отримуємо елемент за його класом
+const circle = document.querySelector('.circle');
+
+// Додаємо слухача подій на клік
+circle.addEventListener('click', toggleIcon);
+
+function toggleIcon() {
+  // Отримуємо елементи із потрібними класами
+  const circleIcon = circle.querySelector('.circle__icon');
+  const plusIcon = circleIcon.querySelector('.circle__plus');
+  const minusIcon = circleIcon.querySelector('.circle__minus');
+
+  // Змінюємо значення стилів, щоб показати "+" або "-"
+  if (plusIcon.style.display === 'none') {
+    plusIcon.style.display = '';
+    minusIcon.style.display = 'none';
+  } else {
+    plusIcon.style.display = 'none';
+    minusIcon.style.display = '';
+  }
+}
+
+// Встановлюємо "+" на початку
+circle.innerHTML = '<div class="circle__icon"><div class="circle__plus" style="display: none">+</div><div class="circle__minus">-</div></div>';
